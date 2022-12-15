d83fa1a18d21bb8d9294e8be3d09f951c78d7a85
phumcherm <kittiphum1112@gmail.com>
2022-12-15 15:08:03 +0700

    borrow

---
+++ public/navbar.php
@@ -0,0 +1,23 @@
+<header class="w3-container w3-top w3-hide-large w3-blue w3-xlarge w3-padding">
+    <a href="javascript:void(0)" class="w3-button w3-blue w3-margin-right" onclick="w3_open()">☰</a>
+    <span>ศูนย์นวัตกรรมและเทคโนโลยีการศึกษา</span>
+</header>
+<!-- Navbar (sit on top) -->
+<nav style="padding: 15px;">
+        <h5>
+            <ul class="menu" id="menu" style="margin: 0;">
+                <li class="items button"><a href="#" onclick="w3_close()">หน้าหลัก</a></li>
+                <li class="items button"><a href="#showcase" onclick="w3_close()">ยืมวัสดุ ครุภัณฑ์</a></li>
+                <li class="items button"><a href="#services" onclick="w3_close()">คืนวัสดุ ครุภัณฑ์</a></li>
+                <li class="items button"><a href="#designers" onclick="w3_close()">คลังวัสดุ ครุภัณฑ์</a></li>
+                <li class="items button"><a href="view/login.php" onclick="w3_close()">ออกจากระบบ</a></li>
+            </ul>
+        </h5>
+
+
+        <div class="w3-bar w3-padding " style="width: auto; margin-left: auto;">
+            <h2 style="margin-left: 80px;">E - Borrow
+                <!-- Right-sided navbar links. Hide them on small screens -->
+            </h2>
+        </div>
+    </nav>
