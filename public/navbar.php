73408d9138ed7158e9d319b9c24c40abfe9a5d7a
phumcherm <kittiphum1112@gmail.com>
2022-12-15 15:48:34 +0700

    responsive

--- public/navbar.php
+++ public/navbar.php
@@ -1,7 +1,7 @@
-<header class="w3-container w3-top w3-hide-large w3-blue w3-xlarge w3-padding">
+<!-- <header class="w3-container w3-top w3-hide-large w3-blue w3-xlarge w3-padding">
     <a href="javascript:void(0)" class="w3-button w3-blue w3-margin-right" onclick="w3_open()">☰</a>
     <span>ศูนย์นวัตกรรมและเทคโนโลยีการศึกษา</span>
-</header>
+</header> -->
 <!-- Navbar (sit on top) -->
 <nav style="padding: 15px;">
         <h5>
