3bddaed366b3e73faa118525f18362e38153cf03
phumcherm <kittiphum1112@gmail.com>
2022-12-16 11:20:44 +0700

    Update login.php

--- public/login.php
+++ public/login.php
@@ -100,17 +100,7 @@
                     <div class=" colororage ">
                         <h3><b>เข้าสู่ระบบ</b></h3>
                         <!-- Email input -->
-<<<<<<< HEAD
-                        <div class="row">
-                            <label for="username">Username</label>
-                            <input type="text" required name="" class="form-control" placeholder="Username">
-                        </div>
-                        <br>
-                        <!-- Password input -->
-                        <div class="row">
-                            <label for="password">Password</label>
-                            <input type="password" required name="" class="form-control " placeholder="Password">
-=======
+
                         <div>
                             <input type="text" required name="txt_code" minlength="13" maxlength="13" class="form-control form-control-lg" placeholder="หมายเลขบัตรประชาชน">
                         </div>
@@ -118,7 +108,6 @@
                         <!-- Password input -->
                         <div>
                             <input type="password" required name="txt_password" minlength="11" maxlength="11" class="form-control form-control-lg" placeholder="รหัสผ่าน (หมายเลขประจำตัว)">
->>>>>>> a471e441759d377a58d1b76efef1919ce0c486ea
                         </div>
                     </div>
                 </form>
