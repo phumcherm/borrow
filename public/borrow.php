e23224951dad1e535f7348e73ba5272c8f7b16c6
phumcherm <kittiphum1112@gmail.com>
2022-12-16 12:05:54 +0700

    borrow

--- public/borrow.php
+++ public/borrow.php
@@ -11,19 +11,41 @@
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
+<style>
+    input {
+        border-radius: 7px;
+        border: none;
+        width: 200px;
+        height: 40px;
+        padding: 20px;
+        margin: 15px;
+    }
+</style>
 
 <body>
     <?php
     include "navbar.php";
     ?>
 
-    <div class="row">
+    <div class="">
 
         <div style="background-color: #dbd6d6;width: auto; height: 500px;margin: 15px;border-radius: 7px;padding: 30px;">
-            <div>
-                <h2 style="color: #ff5722;font-family: SUT_Bold;">
-                    ▶ ยืมวัสดุ ครุภัณฑ์  
-                </h2>
+            <h2 style="color: #ff5722;font-family: SUT_Bold;">
+                ▶ ยืมวัสดุ ครุภัณฑ์
+            </h2>
+            <div style="max-width: 800px; margin: auto; background-color: #ff5722; border-radius: 7px;">
+                <form action="">
+                    <center>
+                        <input type="text" placeholder="ระบุงานที่จะนำไปใช้ทำกิจกรรม">
+                        <div class="row1" style="max-width: 650px;padding: 15px auto 15px;">
+                            <input type="text" placeholder="ระบุสถานที่" style="width: 300px;">
+
+                            <input class="w3-input w3-animate-input" style="width: 300px;" type="date" id="date" name="date" required>
+
+                        </div>
+
+                    </center>
+                </form>
             </div>
         </div>
     </div>
