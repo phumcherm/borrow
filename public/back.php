a471e441759d377a58d1b76efef1919ce0c486ea
phumcherm <kittiphum1112@gmail.com>
2022-12-16 11:13:47 +0700

    r

---
+++ public/back.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>คืนวัสดุ ครุภัณฑ์</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <?php
+    include "navbar.php";
+    ?>
+
+    <div class="row">
+
+        <div style="background-color: #dbd6d6;width: auto; height: 500px;margin: 15px;border-radius: 7px;padding: 30px;">
+            <div>
+                <h2 style="color: #ff5722;font-family: SUT_Bold;">
+                    ▶ คืนวัสดุ ครุภัณฑ์  
+                </h2>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
