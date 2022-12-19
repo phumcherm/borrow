ff728d6b036bf5e48442779988ab41e3d537ae64
phumcherm <kittiphum1112@gmail.com>
2022-12-19 11:49:33 +0700

    navv

--- public/navbar.php
+++ public/navbar.php
@@ -16,7 +16,7 @@
 
 
         <div class="w3-bar w3-padding " style="width: auto; margin-left: auto;">
-            <h2 style="margin-left: 80px;">E - Borrow
+            <h2 style="margin-left: 80px; color: white;">E - Borrow
                 <!-- Right-sided navbar links. Hide them on small screens -->
             </h2>
         </div>
