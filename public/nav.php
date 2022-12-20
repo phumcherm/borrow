7334f622fb13191e68bd5f6dbc1b19661f1862ef
tanchira <57671539+tanchira@users.noreply.github.com>
2022-12-20 16:35:04 +0700

    Update nav.php

--- public/nav.php
+++ public/nav.php
@@ -355,11 +355,7 @@ require_once "../app/model/server.php";
                     <a class="nav-link" href="index.php"><i class="fas fa-tachometer-alt"></i>หน้าหลัก</a>
                 </li>
                 <li class="nav-item">
-<<<<<<< HEAD
                     <a class="nav-link" href="borrow.php"><i class="far fa-address-book"></i>ยืมวัสดุ ครุภัณฑ์</a>
-=======
-                   
->>>>>>> 9c09455b220bc7a340d40bd7201938f3c4403295
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="back.php"><i class="far fa-clone"></i>คืนวัสดุ ครุภัณฑ์</a>
