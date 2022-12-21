c397824c16639a0ae4cf913dab0e22068267a83c
tanchira <57671539+tanchira@users.noreply.github.com>
2022-12-21 15:24:21 +0700

    Update detailBorrow.php

--- public/detailBorrow.php
+++ public/detailBorrow.php
@@ -118,11 +118,11 @@
         .section_area_grid {
             grid-template-columns: 1fr;
         }
-    }
+    }*/
 
-    /* center a:hover {
+     center a:hover {
         background-color: #ffa185;
-    } */
+    } 
 </style>
 
 <body>
@@ -137,18 +137,16 @@
     <div>
 
         <div style="background-color: #dbd6d6;width: auto; height: auto;margin: 15px;border-radius: 7px;padding: 30px;">
-            <h2 style="color: #ff5722;font-family: SUT_Bold;">
-                ▶ คืนวัสดุ ครุภัณฑ์
-            </h2>
+           
             <div style="max-width: 1300px; margin: 15px auto 15px auto;background-color: #b3abab; border-radius: 7px;padding: 30px;">
                 <h3 style="color: white;font-family: SUT_Bold;"><i class="far fa-edit"></i>ทำรายการการคืน</h3>
                 <center><a href="#" style="padding-left: 40px;padding-right:40px;" type="button" data-toggle="modal" data-target="#exampleModal"> Scan QR Code</a></center>
                 <br>
                 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div class="modal-dialog modal-lg">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Appointment Informations</h5>
+                        <div class="modal-content ">
+                            <div class="modal-header ">
+                           <h5 class="modal-title" id="exampleModalLabel">รายละเอียดการจอง</h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true"><i class="fa fa-close"></i></span>
                                 </button>
@@ -158,198 +156,8 @@
 
                                 <div class="row g-0">
 
-                                    <div class="col-md-8 border-right">
-
-                                        <div class="status p-3">
-
-                                            <table class="table table-borderless">
-
-                                                <tbody>
-                                                    <tr>
-
-                                                        <td>
-                                                            <div class="d-flex flex-column">
-
-                                                                <span class="heading d-block">Hospital</span>
-                                                                <span class="subheadings">Cairo Hospital</span>
-
-
-                                                            </div>
-                                                        </td>
-                                                        <td>
-                                                            <div class="d-flex flex-column">
-
-                                                                <span class="heading d-block">Time/Date</span>
-                                                                <span class="subheadings">5:00PM 3-12-2020</span>
-
-
-                                                            </div>
-                                                        </td>
-                                                        <td>
-                                                            <div class="d-flex flex-column">
-
-                                                                <span class="heading d-block">Status</span>
-                                                                <span class="subheadings"><i class="dots"></i> Booked</span>
-
-
-                                                            </div>
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-
-                                                        <td>
-                                                            <div class="d-flex flex-column">
-
-                                                                <span class="heading d-block">Speciality</span>
-                                                                <span class="subheadings">Dental Clinic</span>
-
-
-                                                            </div>
-                                                        </td>
-                                                        <td>
-                                                            <div class="d-flex flex-column">
-
-                                                                <span class="heading d-block">Referring Doctor</span>
-                                                                <span class="subheadings">Dr. Harry Pimn</span>
-
-
-                                                            </div>
-                                                        </td>
-                                                        <td>
-
-
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>
-                                                            <div class="d-flex flex-column">
-                                                                <span class="heading d-block">Contact</span>
-                                                                <span class="subheadings">52, Maria Block, Victoria Road, CA USA</span>
-                                                            </div>
-                                                        </td>
-
-                                                        <td colspan="2">
-
-                                                            <div class="d-flex flex-column">
-                                                                <span class="heading d-block">Reason of visiting</span>
-                                                                <span class="subheadings">Lorem ipsum is placeholder text commonly used in the graphic, print.</span>
-                                                            </div>
-                                                        </td>
-
-                                                    </tr>
-
-                                                    <tr>
-                                                        <td>
-                                                            <div class="d-flex flex-column">
-                                                                <span class="heading d-block">Direction</span>
-                                                                <span class="d-block subheadings">Get direction by using</span>
-                                                                <span class="d-flex flex-row">
-
-                                                                    <img src="https://img.icons8.com/color/100/000000/google-maps.png" class="rounded" width="30" />
-
-                                                                    <img src="https://img.icons8.com/color/100/000000/pittsburgh-map.png" class="rounded" width="30" />
-
-                                                                </span>
-
-                                                            </div>
-                                                        </td>
-
-                                                        <td colspan="2">
-
-                                                            <div class="d-flex flex-column">
-                                                                <span class="heading d-block">Hospital Gallary</span>
-                                                                <span class="d-flex flex-row gallery">
-
-                                                                    <img src="https://i.imgur.com/VfRSLTm.jpg" width="50" class="rounded">
-                                                                    <img src="https://i.imgur.com/jb9Cy5h.jpg" width="50" class="rounded">
-                                                                    <img src="https://i.imgur.com/vBUz4HA.jpg" width="50" class="rounded">
-
-                                                                </span>
-                                                            </div>
-                                                        </td>
-
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-
-
-
-                                        </div>
-
-
-
-
-                                    </div>
-                                    <div class="col-md-4">
-
-                                        <div class="p-2 text-center">
-
-                                            <div class="profile">
-
-                                                <img src="https://i.imgur.com/VfRSLTm.jpg" width="100" class="rounded-circle img-thumbnail">
-
-                                                <span class="d-block mt-3 font-weight-bold">Dr. Samsung Philip.</span>
-
-                                            </div>
-
-                                            <div class="about-doctor">
-
-                                                <table class="table table-borderless">
-
-                                                    <tbody>
-                                                        <tr>
-                                                            <td>
-                                                                <div class="d-flex flex-column">
-
-                                                                    <span class="heading d-block">Education</span>
-                                                                    <span class="subheadings">University of Harward</span>
-
-
-                                                                </div>
-                                                            </td>
-
-                                                            <td>
-                                                                <div class="d-flex flex-column">
-
-                                                                    <span class="heading d-block">Language</span>
-                                                                    <span class="subheadings">Spanish, English</span>
-
-
-                                                                </div>
-                                                            </td>
-                                                        </tr>
-
-
-                                                        <tr>
-                                                            <td>
-                                                                <div class="d-flex flex-column">
-
-                                                                    <span class="heading d-block">Organisation</span>
-                                                                    <span class="subheadings">Accupunture</span>
-
-
-                                                                </div>
-                                                            </td>
-
-                                                            <td>
-                                                                <div class="d-flex flex-column">
-
-                                                                    <span class="heading d-block">Specialist</span>
-                                                                    <span class="subheadings">Accupunture</span>
-
-
-                                                                </div>
-                                                            </td>
-                                                        </tr>
-                                                    </tbody>
-                                                </table>
-
-                                            </div>
-
-                                        </div>
-
-                                    </div>
-
+                                  
+                               
 
 
                                 </div>
