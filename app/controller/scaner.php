837fb1397718347b232e91d21f7931a9922312e7
phumcherm <kittiphum1112@gmail.com>
2022-12-21 14:05:02 +0700

    move file

---
+++ app/controller/scaner.php
@@ -0,0 +1,64 @@
+<script src="./node_modules/html5-qrcode/html5-qrcode.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html5-qrcode/2.3.4/html5-qrcode.min.js" integrity="sha512-k/KAe4Yff9EUdYI5/IAHlwUswqeipP+Cp5qnrsUjTPCgl51La2/JhyyjNciztD7mWNKLSXci48m7cctATKfLlQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<style>
+    main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #reader {
+        width: 600px;
+    }
+
+    #result {
+        text-align: center;
+        font-size: 1.5rem;
+    }
+
+    #html5-qrcode-button-camera-start {
+        width: 400px;
+        /* content: "  เปิดกล้อง "; */
+        background-color: green;
+        border-radius: 7px;
+    }
+</style>
+<main>
+    <div id="reader"></div>
+    <div id="result"></div>
+</main>
+<script>
+    const scanner = new Html5QrcodeScanner('reader', {
+        qrbox: {
+            width: 250,
+            height: 250,
+            // textBox: "เปิด"
+        },
+        fps: 20,
+
+    });
+    scanner.render(success, error);
+
+    function success(result) {
+        const itemCode = document.getElementById("result");
+
+        document.getElementById('result').innerHTML = `
+                                <h2>Success!</h2>
+                                <a href="borrow.php?code=${result}"> ${result}</a>
+                                `;
+
+        // var itemCode = $result
+        // document.getElementById("demo").innerHTML = (result);
+        // document.(itemCode);
+        scanner.clear();
+        document.getElementById('reader').remove();
+        // echo  (itemCode);
+    }
+
+    function error(err) {
+        console.error(err);
+    }
+    // <?php echo "หวัดดี"  ?>
+</script>
--- public/borrow.php
+++ public/borrow.php
@@ -15,10 +15,7 @@
     <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="./node_modules/html5-qrcode/html5-qrcode.min.js"></script>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5-qrcode/2.3.4/html5-qrcode.min.js" integrity="sha512-k/KAe4Yff9EUdYI5/IAHlwUswqeipP+Cp5qnrsUjTPCgl51La2/JhyyjNciztD7mWNKLSXci48m7cctATKfLlQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
+    
 </head>
 <style>
     input {
@@ -76,11 +73,11 @@
 </style>
 
 <body>
-    
+
     <button onclick="topFunction()" id="myBtn" title="Go to top" style="opacity: 0.5;background-color: #ff5722;width: 50px; height: 50px;"><i class="fas fa-chevron-circle-up"></i></button>
-    <button type="button" class="btn btn-danger btn-floating btn-lg" id="btn-back-to-top">
+    <!-- <button type="button" class="btn btn-danger btn-floating btn-lg" id="btn-back-to-top">
         <i class="fas fa-arrow-up"></i>
-    </button>
+    </button> -->
     <?php
     include "nav.php";
     ?>
@@ -94,62 +91,15 @@
             <div style="max-width: 1300px; margin: 15px auto 15px auto;background-color: #b3abab; border-radius: 7px;padding: 30px;">
                 <h3 style="color: white;font-family: SUT_Bold;"><i class="far fa-edit"></i>ทำรายการ</h3>
                 <form action="">
-                    <style>
-                        main {
-                            display: flex;
-                            justify-content: center;
-                            align-items: center;
-                        }
-
-                        #reader {
-                            width: 600px;
-                        }
-
-                        #result {
-                            text-align: center;
-                            font-size: 1.5rem;
-                        }
-                    </style>
-                    <main>
-                        <div id="reader"></div>
-                        <div id="result"></div>
-                    </main>
-                    <script>
-                        const scanner = new Html5QrcodeScanner('reader', {
-                            qrbox: {
-                                width: 250,
-                                height: 250,
-                            },
-                            fps: 20,
-                        });
-                        scanner.render(success, error);
-
-                        function success(result) {
-                            var itemCode = document.getElementById("result");
-                            document.getElementById('result').innerHTML = `
-                                <h2>Success!</h2>
-                                <a href="borrow.php?code=${result}"> ${result}</a>
-                                `;
-
-                            // var itemCode = $result
-                            // document.getElementById("demo").innerHTML = (result);
-                            // document.(itemCode);
-                            scanner.clear();
-                            document.getElementById('reader').remove();
-                            // echo  (itemCode);
-                        }
-
-                        function error(err) {
-                            console.error(err);
-                        }
-                        <?php echo "หวัดดี"  ?>
-                    </script>
+                    <?
+                    require "../app/controller/scaner.php"
+                    ?>
                     <!-- <center><a href="#"> Scan QR Code</a></center> -->
-                    <? 
-
-                    if (isset( $_GET['code'])) {
-                        echo $_GET['code']; 
-                    }?>
+                    <?
+                    // echo $itemCode;
+                    if (isset($_GET['code'])) {
+                        echo $_GET['code'];
+                    } ?>
                     <br>
                     <center>
 
@@ -171,48 +121,9 @@
                             </div>
                         </div>
                     </center>
-                    <div style="max-width: 1600px;margin-left: auto;">
-                        <!-- <h2 style="padding-left: 200px;">รายละเอียดการยืม</h2> -->
-                        <table class="table" style="max-width: 1200px;margin: auto; padding: 16px;background-color: white;border-radius: 7px;">
-                            <thead class="table-dark">
-                                <th>
-                                    <center>ลำดับ</center>
-                                </th>
-                                <th>
-                                    <center>รายการ</center>
-                                </th>
-                                <th>
-                                    <center>ห้อง-แผนก</center>
-                                </th>
-                                <th>
-                                    <center>วันที่ยืม</center>
-                                </th>
-                                <th>
-                                    <center>วันที่คืน</center>
-                                </th>
-                            </thead>
-                            <?php for ($i = 0; $i < 5; $i++) {
-                            ?>
-                                <tbody>
-                                    <td>
-                                        <center><?php echo $i ?></center>
-                                    </td>
-                                    <td>
-                                        <center>สาย HDMI </center>
-                                    </td>
-                                    <td>
-                                        <center>ศนท.</center>
-                                    </td>
-                                    <td>
-                                        <center>12-12-2565</center>
-                                    </td>
-                                    <td>
-                                        <center>30-12-2565</center>
-                                    </td>
-                                </tbody>
-                            <?php } ?>
-                        </table>
-                    </div>
+                    
+                    <? require "../app/models/table.php"?>
+
                     <br>
                     <button type="submit">ยืนยันทั้งหมด</button>
                 </form>
@@ -227,29 +138,8 @@
 
 </body>
 <script>
-    // Get the button
-    // let mybutton = document.getElementById("myBtn");
-
-    // // When the user scrolls down 20px from the top of the document, show the button
-    // window.onscroll = function() {
-    //     scrollFunction()
-    // };
-
-    // function scrollFunction() {
-    //     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    //         mybutton.style.display = "block";
-    //     } else {
-    //         mybutton.style.display = "none";
-    //     }
-    // }
-
-    // // When the user clicks on the button, scroll to the top of the document
-    // function topFunction() {
-    //     document.body.scrollTop = 0;
-    //     document.documentElement.scrollTop = 0;
-    // }
+    
 </script>
-<!-- <script src="script.js"></script> -->
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
