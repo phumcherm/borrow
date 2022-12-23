10049e25dc9ea72fc112582c1de79ff66eca27c7
phumcherm <kittiphum1112@gmail.com>
2022-12-23 14:04:32 +0700

    Update index.php

--- public/admin/index.php
+++ public/admin/index.php
@@ -6,7 +6,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <!-- <link rel="stylesheet" href="borrow/public/css/style.css"> -->
+    <!-- <link rel="stylesheet" href="/borrow/public/css/style.css"> -->
+    <link rel="stylesheet" type="text/css" href="../../public/css/style.css">
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 
