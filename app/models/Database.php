225f269985381ab61fd78dd207b5e6c3c3dc4e78
tanchira <57671539+tanchira@users.noreply.github.com>
2023-01-12 11:06:16 +0700

    Update Database.php

--- app/models/Database.php
+++ app/models/Database.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< HEAD
 $servername = "172.19.0.1:9906";
 $username = "ceitdb";
 $password = "12345678";
@@ -18,7 +19,20 @@ $password = "12345678";
     } catch(PDOException $e) {
         $e->getMessage();
     }
+=======
+$db_host = "172.17.0.1:9906"; //202.28.34.205
+$db_user = "ceitdb"; //62011211097
+$db_password = "12345678"; //62011211097
+$db_name = "ceitdb"; //62011211097
+>>>>>>> a0352478f648c165eee74e0bb57e0c8ca250dfa4
 
+try {
+    $db = new PDO("mysql:host={$db_host}; dbname={$db_name}", $db_user, $db_password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOEXCEPTION $e) {
+    $e->getMessage();
+}
+date_default_timezone_set('Asia/Bangkok');
 
 ?>
 
