036bc26b2f85d51d9ceb79c283c5740cd972b1cd
tanchira <57671539+tanchira@users.noreply.github.com>
2023-01-23 14:03:03 +0700

    Update Database.php

--- app/models/Database.php
+++ app/models/Database.php
@@ -1,11 +1,11 @@
 <?php
 
-<<<<<<< HEAD
-$servername = "172.19.0.1:9906";
-$username = "ceitdb";
-$password = "12345678";
+$db_host = "172.17.0.1:9906"; //202.28.34.205
+$db_user = "ceitdb"; //62011211097
+$db_password = "12345678"; //62011211097
+$db_name = "ceitdb"; //62011211097
 	try {
-        $conn = new PDO("mysql:host=$servername;dbname=ceitdb", $username, $password);
+        $conn = new PDO("mysql:host=$db_host;dbname=ceitdb", $db_user, $db_password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         //  echo "Connected successfully";
@@ -14,17 +14,11 @@ $password = "12345678";
     }
    
     try {
-        $db = new PDO("mysql:host=$servername;dbname=ceitdb", $username, $password);
+        $db = new PDO("mysql:host=$db_host;dbname=ceitdb", $db_user, $db_password);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e) {
         $e->getMessage();
     }
-=======
-$db_host = "172.17.0.1:9906"; //202.28.34.205
-$db_user = "ceitdb"; //62011211097
-$db_password = "12345678"; //62011211097
-$db_name = "ceitdb"; //62011211097
->>>>>>> a0352478f648c165eee74e0bb57e0c8ca250dfa4
 
 try {
     $db = new PDO("mysql:host={$db_host}; dbname={$db_name}", $db_user, $db_password);
