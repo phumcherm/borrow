777c18a3d6bb4b4d16cddacfc0aa149b15215613
tanchira <57671539+tanchira@users.noreply.github.com>
2023-01-25 13:09:17 +0700

    Update index.php

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,8 +1,5 @@
 <?php
-// require_once "../app/models/Database.php";
 require_once "../../app/models/function.php";
-/* // require_once "../app/models/Database.php"; */
-
 
 $con = mysqli_connect("172.19.0.1:9906", "ceitdb", "12345678", "ceitdb");
 
@@ -30,14 +27,9 @@ $result_l = mysqli_query($con, $query);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-
     <link rel="stylesheet" href="/borrow/public/css/style.css">
     <link rel="stylesheet" href="/borrow/public/css/icons.png">
-
-
-
     <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
-
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <style>
@@ -82,18 +74,9 @@ $result_l = mysqli_query($con, $query);
 </head>
 
 <body>
-
-
-    <!-- <header class="w3-container w3-top w3-hide-large w3-blue w3-xlarge w3-padding">
-        <a href="javascript:void(0)" class="w3-button w3-blue w3-margin-right" onclick="w3_open()">☰</a>
-        <span>ศูนย์นวัตกรรมและเทคโนโลยีการศึกษา</span> 
-    </header> -->
-
-    <!-- Navbar (sit on top) -->
     <?php
     include "nav_admin.php";
     ?>
-
 
     <div style="background-color: #827A7A;width: auto; height: auto;margin: 15px;border-radius: 7px;padding: 30px; box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 10px;">
         <div>
@@ -107,6 +90,7 @@ $result_l = mysqli_query($con, $query);
         <center>
             <input class="w3-input " type="text" id="myInput" onkeyup="myFunction()" placeholder="Search .." style="max-width: 100%; max-height: 100%;margin: 15px;border-radius: 7px;padding: 30px; box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 10px;" title="Type in a name">
         </center>
+
         <div class='pagination-container'>
             <p Align=right>
             <nav aria-label="Page navigation example">
@@ -126,8 +110,6 @@ $result_l = mysqli_query($con, $query);
                 </ul>
             </nav>
             </p>
-
-
             <br><br><br>
             <p Align=right>
                 <select name="state" id="maxRows" style=" border-color:#827A7A; box-shadow: 0px 0px 0px 6px rgba(255, 255, 255, 0.3); ">
@@ -141,150 +123,184 @@ $result_l = mysqli_query($con, $query);
                     <option value="100">100</option>
                 </select>
             </p>
+        </div><br>
 
-        </div>
-
-
-        <br>
         <div class="table-responsive">
-            <div >
+            <div>
                 <table id="datatable" class="table" style="max-width: 1200px;margin: auto; padding: 16px;background-color: white;border-radius: 7px;text-align: center; box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 10px;">
-                    <thead class="table-dark">
-                        <th>
-                            <center>id </center>
-                        </th>
-                        <th>
-                            <center>updateTime</center>
-                        </th>
-                        <th>
-                            <center>itemCode</center>
-                        </th>
-                        <th>
-                            <center>detail</center>
-                        </th>
-                        <th>
-                            <center>checkInDate</center>
-                        </th>
-                        <th>
-                            <center>brand</center>
-                        </th>
-                        <th>
-                            <center>serialNumber</center>
-                        </th>
-                        <th>
-                            <center>price</center>
-                        </th>
-                        <th>
-                            <center>refDoc</center>
-                        </th>
-                        <th>
-                            <center>room</center>
-                        </th>
-                        <th>
-                            <center>status</center>
-                        </th>
-
-                    </thead>
-                    <?php
+                    <div>
 
 
-                    $selectAll = new DB_con();
-                    $sql = $selectAll->selectAll();
+                        <thead class="table-dark">
+                            <th>
+                                <center>id </center>
+                            </th>
+                            <th>
+                                <center>updateTime</center>
+                            </th>
+                            <th>
+                                <center>itemCode</center>
+                            </th>
+                            <th>
+                                <center>detail</center>
+                            </th>
+                            <th>
+                                <center>checkInDate</center>
+                            </th>
+                            <th>
+                                <center>brand</center>
+                            </th>
+                            <th>
+                                <center>serialNumber</center>
+                            </th>
+                            <th>
+                                <center>price</center>
+                            </th>
+                            <th>
+                                <center>refDoc</center>
+                            </th>
+                            <th>
+                                <center>room</center>
+                            </th>
+                            <th>
+                                <center>status</center>
+                            </th>
+                            <!-- <th>
+                     <center>status</center>
+                 </th>
+                 <th>
+                     <center>notation</center>
+                 </th>
+                 <th>
+                     <center>misConfirmer</center>
+                 </th>
+                 <th>
+                     <center>organization</center>
+                 </th>
+                 <th>
+                     <center>type</center>
+                 </th>
+                 <th>
+                     <center>active</center>
+                 </th> -->
 
+                        </thead>
 
-                    while ($row = mysqli_fetch_assoc($result_l)) {
-
-                    ?>
                         <tbody>
-
-                            <td>
-                                <center><?php echo $row["id"] ?></center>
-                            </td>
-                            <td>
-                                <center><?php echo $row["updateTime"] ?></center>
-                            </td>
-                            <td>
-                                <center><?php echo $row["itemCode"] ?></center>
-                            </td>
-                            <td>
-                                <center><?php echo $row["detail"] ?></center>
-                            </td>
-                            <td>
-                                <center><?php echo $row["checkInDate"] ?></center>
-                            </td>
-                            <td>
-                                <center><?php echo $row["brand"] ?></center>
-                            </td>
-                            <td>
-                                <center><?php echo $row["serialNumber"] ?></center>
-                            </td>
-                            <td>
-                                <center><?php echo $row["price"] ?></center>
-                            </td>
-                            <td>
-                                <center><?php echo $row["refDoc"] ?></center>
-                            </td>
-                            <td>
-                                <center><?php echo $row["room"] ?></center>
-                            </td>
-                            <td>
-                                <center><?php echo $row["status"] ?></center>
-                            </td>
+                            <?php
 
 
+                            $selectAll = new DB_con();
+                            $sql = $selectAll->selectAll();
+                            while ($row = mysqli_fetch_array($sql)) {
+                            ?>
+                                <td>
+                                    <center><?php echo $row["id"] ?></center>
+                                </td>
+                                <td>
+                                    <center> <?php echo $row["updateTime"] ?></center>
+                                </td>
+                                <td>
+                                    <center> <?php echo $row["itemCode"] ?></center>
+                                </td>
+
+                                <td>
+                                    <center><?php echo $row["detail"] ?></center>
+                                </td>
+                                <td>
+                                    <?php echo $row["checkInDate"] ?>
+                                </td>
+                                <td>
+                                    <center><?php echo $row["brand"] ?></center>
+                                </td>
+                                <td>
+                                    <center> <?php echo $row["serialNumber"] ?></center>
+                                </td>
+                                <td>
+                                    <center> <?php echo $row["price"] ?></center>
+                                </td>
+                                <td>
+                                    <center> <?php echo $row["refDoc"] ?></center>
+                                </td>
+                                <td>
+                                    <center><?php echo $row["room"] ?></center>
+                                </td>
+                                <td>
+                                    <center><?php echo $row["status"] ?></center>
+                                </td>
+                                <!-- <td>
+                     <center><?php echo $row["status"] ?></center>
+                 </td>
+                 <td>
+                     <center><?php echo $row["notation"] ?></center>
+                 </td>
+                 <td>
+                     <center><?php echo $row["misConfirmer"] ?></center>
+                 </td>
+                 <td>
+                     <center><?php echo $row["organization"] ?></center>
+                 </td>
+                 <td>
+                     <center><?php echo $row["type"] ?></center>
+                 </td>
+                  -->
                         </tbody>
                     <?php } ?>
                 </table>
 
-            </div>
+
+
             </div>
         </div>
+    </div>
 
-        <!-- Sidebar/menu -->
 
-        <!-- Top menu on small screens -->
-        <script>
-            // Script to open and close sidebar
-            function w3_open() {
-                document.getElementById("mySidebar").style.display = "block";
-                document.getElementById("myOverlay").style.display = "block";
-            }
 
-            function w3_close() {
-                document.getElementById("mySidebar").style.display = "none";
-                document.getElementById("myOverlay").style.display = "none";
-            }
 
-            // Modal Image Gallery
-            function onClick(element) {
-                document.getElementById("img01").src = element.src;
-                document.getElementById("modal01").style.display = "block";
-                var captionText = document.getElementById("caption");
-                captionText.innerHTML = element.alt;
-            }
-        </script>
-        <script>
-            function myFunction() {
-                var input, filter, table, tr, td, i, txtValue;
-                input = document.getElementById("myInput");
-                filter = input.value.toUpperCase();
-                table = document.getElementById("datatable");
+    <script>
+        // Script to open and close sidebar
+        function w3_open() {
+            document.getElementById("mySidebar").style.display = "block";
+            document.getElementById("myOverlay").style.display = "block";
+        }
 
-                tr = table.getElementsByTagName("tr");
-                for (i = 0; i < tr.length; i++) {
-                    td = tr[i].getElementsByTagName("td")[3];
-                    if (td) {
-                        txtValue = td.textContent || td.innerText;
-                        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                            tr[i].style.display = "";
-                        } else {
-                            tr[i].style.display = "none";
-                        }
+        function w3_close() {
+            document.getElementById("mySidebar").style.display = "none";
+            document.getElementById("myOverlay").style.display = "none";
+        }
+
+        // Modal Image Gallery
+        function onClick(element) {
+            document.getElementById("img01").src = element.src;
+            document.getElementById("modal01").style.display = "block";
+            var captionText = document.getElementById("caption");
+            captionText.innerHTML = element.alt;
+        }
+    </script>
+
+    <script>
+        function myFunction() {
+            var input, filter, table, tr, td, i, txtValue;
+            input = document.getElementById("myInput");
+            filter = input.value.toUpperCase();
+            table = document.getElementById("datatable");
+
+            tr = table.getElementsByTagName("tr");
+            for (i = 0; i < tr.length; i++) {
+                td = tr[i].getElementsByTagName("td")[3];
+                if (td) {
+                    txtValue = td.textContent || td.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    } else {
+                        tr[i].style.display = "none";
                     }
                 }
             }
-        </script>
-          <script>
+        }
+    </script>
+
+    <script>
         getPagination('#datatable');
 
         function getPagination(table) {
