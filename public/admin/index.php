0854dd0182ba991087040b23dcaed64f3422271f
tanchira <57671539+tanchira@users.noreply.github.com>
2023-02-02 14:07:41 +0700

    Update index.php

--- public/admin/index.php
+++ public/admin/index.php
@@ -20,10 +20,7 @@ $con = mysqli_connect("172.19.0.1:9906", "ceitdb", "12345678", "ceitdb");
 <html lang="en">
 
 <head>
-<<<<<<< HEAD
-=======
     <title>E - Borrow || ADMIN</title>
->>>>>>> 905bad3efbeab67a54475a32be54aec9d4c9ebfc
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -85,114 +82,15 @@ $con = mysqli_connect("172.19.0.1:9906", "ceitdb", "12345678", "ceitdb");
                                 <th>สถานะ</th>
                                 <th>เจ้าหน้าที่</th>
 
-<<<<<<< HEAD
-                            </tr>
-=======
-        <center>
-            <input class="w3-input " type="text" id="myInput" onkeyup="myFunction()" placeholder="Search .." style="max-width: 100%; max-height: 100%;margin: 15px;border-radius: 7px;padding: 30px; box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 10px;" title="Type in a name">
-        </center>
+      
 
-        <div class='pagination-container'>
-            <p Align=right>
-            <nav aria-label="Page navigation example">
-                <ul class="pagination">
+    
 
-                    <li data-page="prev" class="page-item">
-                        <a class="page-link" href="#" style=" border-color:#827A7A; color:#000;">Previous
-                            <span>
-                                <span class="sr-only">(current)
-                                </span></a>
-                    </li>
-                    <!--	Here the JS Function Will Add the Rows -->
-                    <li data-page="next" class="page-item">
-                        <a class="page-link" href="#" style=" border-color:#827A7A; color:#000;">Next
-                            <span> <span class="sr-only">(current)</span></span></a>
-                    </li>
-                </ul>
-            </nav>
-            </p>
-            <br><br><br>
-            <p Align=right>
-                <select name="state" id="maxRows" style=" border-color:#827A7A; box-shadow: 0px 0px 0px 6px rgba(255, 255, 255, 0.3); ">
-                    <option value="5000">Show ALL Rows</option>
-                    <option value="5">5</option>
-                    <option value="10">10</option>
-                    <option value="15">15</option>
-                    <option value="20">20</option>
-                    <option value="50">50</option>
-                    <option value="70">70</option>
-                    <option value="100">100</option>
-                </select>
-            </p>
-        </div><br>
+    
 
-        <div class="table-responsive" style="padding: 25px;">
-            <div>
-                <table id="datatable" class="table" style="max-width: 1200px;margin: auto; padding: 16px;background-color: white;border-radius: 7px;text-align: center; box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 10px;">
-                    <div>
-
-
-                        <thead class="table-dark">
-                            <th>
-                                <center>id </center>
-                            </th>
-                            <th>
-                                <center>updateTime</center>
-                            </th>
-                            <th>
-                                <center>itemCode</center>
-                            </th>
-                            <th>
-                                <center>detail</center>
-                            </th>
-                            <th>
-                                <center>checkInDate</center>
-                            </th>
-                            <th>
-                                <center>brand</center>
-                            </th>
-                            <th>
-                                <center>serialNumber</center>
-                            </th>
-                            <th>
-                                <center>price</center>
-                            </th>
-                            <th>
-                                <center>refDoc</center>
-                            </th>
-                            <th>
-                                <center>room</center>
-                            </th>
-                            <th>
-                                <center>status</center>
-                            </th>
-                            <!-- <th>
-                     <center>status</center>
-                 </th>
-                 <th>
-                     <center>notation</center>
-                 </th>
-                 <th>
-                     <center>misConfirmer</center>
-                 </th>
-                 <th>
-                     <center>organization</center>
-                 </th>
-                 <th>
-                     <center>type</center>
-                 </th>
-                 <th>
-                     <center>active</center>
-                 </th> -->
-
->>>>>>> 905bad3efbeab67a54475a32be54aec9d4c9ebfc
                         </thead>
                         <tbody>
                             <?php
-<<<<<<< HEAD
-=======
-
->>>>>>> 905bad3efbeab67a54475a32be54aec9d4c9ebfc
                             $selectAll = new DB_con();
                             $sql = $selectAll->selectAll();
                             while ($row = mysqli_fetch_array($sql)) {
