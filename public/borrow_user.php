df757bd24275f2ad0f049b165e7c66853af042fd
Tarzar007 <54nawapad@gmail.com>
2023-02-10 14:31:47 +0700

    update

--- public/borrow_user.php
+++ public/borrow_user.php
@@ -137,43 +137,43 @@ $result_l = mysqli_query($con, $query);
 
                 <center>
                     <input class="w3-input" type="text" required placeholder="code" id="data4" name="data4" style="max-width: 500px;visibility: hidden; ">
-                    <<<<<<< HEAD <!-- <h2 id="data4" name="data4">
+                    <!-- <h2 id="data4" name="data4">
                         </h2> -->
-                        <!-- <h2 id="result" ></h2> -->
-                        <!-- <br><br> -->
-                        <input class="w3-input" type="text" required placeholder="ระบุงานที่จะนำไปใช้ทำกิจกรรม" id="activity" name="activity" style="max-width: 500px; box-shadow: rgba(0.35, 0, 0, 0.35) 0px 5px 10px;">
-                        <div class="section_area_grid">
-                            =======
-                            <!--  //<h2 id="data4" name="data4"></h2>
+                    <!-- <h2 id="result" ></h2> -->
+                    <!-- <br><br> -->
+                    <input class="w3-input" type="text" required placeholder="ระบุงานที่จะนำไปใช้ทำกิจกรรม" id="activity" name="activity" style="max-width: 500px; box-shadow: rgba(0.35, 0, 0, 0.35) 0px 5px 10px;">
+                    <div class="section_area_grid">
+
+                        <!--  //<h2 id="data4" name="data4"></h2>
                         //<h2 id="result" ></h2>
                        // <br><br> -->
-                            <div class="col-sm-12 col-md-6 mb-3 mb-lg-0 col-lg-4">
-                                <input class="w3-input" type="text" required placeholder="ระบุงานที่จะนำไปใช้ทำกิจกรรม" id="activity" name="activity" style="max-width: 500px; box-shadow: rgba(0.35, 0, 0, 0.35) 0px 5px 10px;">
-                            </div>
-                            >>>>>>> 40c557a2107a79cb1ce49fbb7e378ccc6f168b39
+                        <div class="col-sm-12 col-md-6 mb-3 mb-lg-0 col-lg-4">
+                            <input class="w3-input" type="text" required placeholder="ระบุงานที่จะนำไปใช้ทำกิจกรรม" id="activity" name="activity" style="max-width: 500px; box-shadow: rgba(0.35, 0, 0, 0.35) 0px 5px 10px;">
+                        </div>
 
-                            <div class="col-sm-12 col-md-6 mb-3 mb-lg-0 col-lg-4">
-                                <div class="section_area_grid">
 
-                                    <div class="section_grid_bor">
-                                        <div class="section_grid_item">
-                                            <!--        //<h5 style="padding-left: 0;">ระบุสถานที่<h5> -->
-                                            <br>
-                                            <input class="w3-input w3-animate-input demo" id="location" name="location" type="text" required placeholder="ระบุสถานที่" style="max-width: 500px; box-shadow: rgba(0.35, 0, 0, 0.35) 0px 5px 10px; ">
-                                        </div>
-                                    </div>
-                                </div>
-                                <!--    // <p id="demo">CODE</p> -->
-                                <br>
-                                <div class="col-sm-12 col-md-6 mb-3 mb-lg-0 col-lg-4">
-                                    <div class="section_grid_bor">
-                                        <div class="section_grid_item">
-                                            <div style="font-size: 25px;">วันที่คืน*</div>
-                                            <input class="w3-input w3-animate-input" style="max-width: 500px; box-shadow: rgba(0.35, 0, 0, 0.35) 0px 5px 10px;" type="date" id="date" name="date" required>
-                                        </div>
+                        <div class="col-sm-12 col-md-6 mb-3 mb-lg-0 col-lg-4">
+                            <div class="section_area_grid">
+
+                                <div class="section_grid_bor">
+                                    <div class="section_grid_item">
+                                        <!--        //<h5 style="padding-left: 0;">ระบุสถานที่<h5> -->
+                                        <br>
+                                        <input class="w3-input w3-animate-input demo" id="location" name="location" type="text" required placeholder="ระบุสถานที่" style="max-width: 500px; box-shadow: rgba(0.35, 0, 0, 0.35) 0px 5px 10px; ">
                                     </div>
                                 </div>
                             </div>
+                            <!--    // <p id="demo">CODE</p> -->
+                            <br>
+                            <div class="col-sm-12 col-md-6 mb-3 mb-lg-0 col-lg-4">
+                                <div class="section_grid_bor">
+                                    <div class="section_grid_item">
+                                        <div style="font-size: 25px;">วันที่คืน*</div>
+                                        <input class="w3-input w3-animate-input" style="max-width: 500px; box-shadow: rgba(0.35, 0, 0, 0.35) 0px 5px 10px;" type="date" id="date" name="date" required>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                 </center>
                 <br><br>
 
@@ -181,10 +181,7 @@ $result_l = mysqli_query($con, $query);
 
 
                 <!--  </div> -->
-
-
-
-                <<<<<<< HEAD <div style="max-width: 1600px;margin-left: auto;">
+                <div style="max-width: 1600px;margin-left: auto;">
                     <!-- <h2 style="padding-left: 200px;">รายละเอียดการยืม</h2> -->
                     <table class="table" style="max-width: 1200px;margin: auto; padding: 16px;background-color: white;border-radius: 7px; box-shadow: rgba(0.35, 0, 0, 0.35) 0px 5px 10px;">
                         <thead class="table-dark">
@@ -228,97 +225,95 @@ $result_l = mysqli_query($con, $query);
                             </p>
                         </tbody>
                     </table>
-        </div>
+                </div>
 
-        <script>
-            function delAll_test() {
-                // document.getElementById("data4").innerHTML = code234
-                // console.log(code234)
-                itemCode.length = 0
-                console.log("del = " + itemCode)
-                tableFunc()
-            }
-        </script>
-        <br>
-        <a type="button" id="cancle" onclick="delAll_test()">ยกเลิก</a>
-        <button type="submit" id="submit" name="submit">ยืนยันทั้งหมด</button>
-        =======
-
-        >>>>>>> 40c557a2107a79cb1ce49fbb7e378ccc6f168b39
-
-
-        <div class="table-responsive">
-            <div>
-                <table id="datatable" class="table" style="max-width: 1200px;margin: auto; padding: 16px;background-color: white;border-radius: 7px;text-align: center; box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 10px;">
+                <script>
+                    function delAll_test() {
+                        // document.getElementById("data4").innerHTML = code234
+                        // console.log(code234)
+                        itemCode.length = 0
+                        console.log("del = " + itemCode)
+                        tableFunc()
+                    }
+                </script>
+                <br>
+                <a type="button" id="cancle" onclick="delAll_test()">ยกเลิก</a>
+                <button type="submit" id="submit" name="submit">ยืนยันทั้งหมด</button>
 
 
 
-                    <div style="max-width: 1600px;margin-left: auto;">
-                        <!-- <h2 style="padding-left: 200px;">รายละเอียดการยืม</h2> -->
-                        <table class="table" style="max-width: 1200px;margin: auto; padding: 16px;background-color: white;border-radius: 7px; box-shadow: rgba(0.35, 0, 0, 0.35) 0px 5px 10px;">
-                            <thead class="table-dark">
-                                <th>
-                                    <center>id </center>
-                                </th>
-                                <th>
-                                    <center>updateTime</center>
-                                </th>
-                                <th>
-                                    <center>itemCode</center>
-                                </th>
-                                <th>
-                                    <center>detail</center>
-                                </th>
-                                <th>
-                                    <center>checkInDate</center>
-                                </th>
-                                <th>
-                                    <center>brand</center>
-                                </th>
-                                <th>
-                                    <center>serialNumber</center>
-                                </th>
-                                <th>
-                                    <center>price</center>
-                                </th>
-                                <th>
-                                    <center>refDoc</center>
-                                </th>
-                                <th>
-                                    <center>room</center>
-                                </th>
-                                <th>
-                                    <center></center>
-                                </th>
-                            </thead>
-                            <tbody id="data">
-                                <p>
-                                    <td colspan="10" class="text-center">แสกน QR Code ยืมครุภัณฑ์</td>
-                                </p>
-                            </tbody>
+                <div class="table-responsive">
+                    <div>
+                        <table id="datatable" class="table" style="max-width: 1200px;margin: auto; padding: 16px;background-color: white;border-radius: 7px;text-align: center; box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 10px;">
+
+
+
+                            <div style="max-width: 1600px;margin-left: auto;">
+                                <!-- <h2 style="padding-left: 200px;">รายละเอียดการยืม</h2> -->
+                                <table class="table" style="max-width: 1200px;margin: auto; padding: 16px;background-color: white;border-radius: 7px; box-shadow: rgba(0.35, 0, 0, 0.35) 0px 5px 10px;">
+                                    <thead class="table-dark">
+                                        <th>
+                                            <center>id </center>
+                                        </th>
+                                        <th>
+                                            <center>updateTime</center>
+                                        </th>
+                                        <th>
+                                            <center>itemCode</center>
+                                        </th>
+                                        <th>
+                                            <center>detail</center>
+                                        </th>
+                                        <th>
+                                            <center>checkInDate</center>
+                                        </th>
+                                        <th>
+                                            <center>brand</center>
+                                        </th>
+                                        <th>
+                                            <center>serialNumber</center>
+                                        </th>
+                                        <th>
+                                            <center>price</center>
+                                        </th>
+                                        <th>
+                                            <center>refDoc</center>
+                                        </th>
+                                        <th>
+                                            <center>room</center>
+                                        </th>
+                                        <th>
+                                            <center></center>
+                                        </th>
+                                    </thead>
+                                    <tbody id="data">
+                                        <p>
+                                            <td colspan="10" class="text-center">แสกน QR Code ยืมครุภัณฑ์</td>
+                                        </p>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                            <script>
+                                function delAll_test() {
+                                    // document.getElementById("data4").innerHTML = code234
+                                    // console.log(code234)
+                                    itemCode.length = 0
+                                    console.log("del = " + itemCode)
+                                    tableFunc()
+                                }
+                            </script>
+                            <br>
+                            <a type="button" id="cancle" onclick="delAll_test()">ยกเลิก</a>
+                            <button type="submit" id="submit" name="submit">ยืนยันทั้งหมด</button>
+
                         </table>
                     </div>
-
-                    <script>
-                        function delAll_test() {
-                            // document.getElementById("data4").innerHTML = code234
-                            // console.log(code234)
-                            itemCode.length = 0
-                            console.log("del = " + itemCode)
-                            tableFunc()
-                        }
-                    </script>
-                    <br>
-                    <a type="button" id="cancle" onclick="delAll_test()">ยกเลิก</a>
-                    <button type="submit" id="submit" name="submit">ยืนยันทั้งหมด</button>
-
-                </table>
-            </div>
+                </div>
+            </form>
+            <!-- </div> -->
+            <br>
         </div>
-        </form>
-        <!-- </div> -->
-        <br>
-    </div>
     </div>
 </body>
 
