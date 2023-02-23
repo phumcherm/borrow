66215a6146cd631f2f8957723b53b35df9dd3c0e
tanchira <57671539+tanchira@users.noreply.github.com>
2023-02-23 16:35:17 +0700

    Update borrow_user.php

--- public/borrow_user.php
+++ public/borrow_user.php
@@ -26,8 +26,8 @@ $result_l = mysqli_query($con, $query);
     <title>ยืมวัสดุ ครุภัณฑ์</title>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/table.css">
+
+   
     <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
@@ -37,8 +37,8 @@ $result_l = mysqli_query($con, $query);
         border: none;
         width: auto;
         height: 40px;
-        padding: 20px;
-        margin: 15px auto 15px auto;
+        padding: 2px;
+        margin: 5px auto 5px auto;
         text-align: center;
     }
 
@@ -85,6 +85,7 @@ $result_l = mysqli_query($con, $query);
     #submit {
         float: right;
         box-shadow: rgba(0, 0, 0, 0.35) 0px 0px 10px;
+        border-radius: 10px;
     }
 
     @media all and (max-width: 800px) {
@@ -107,6 +108,51 @@ $result_l = mysqli_query($con, $query);
         background: linear-gradient(to bottom right, #4F4848, #686060, #827A7A, #CFC7C7);
         /* Standard syntax */
     }
+
+    .graphBox {
+        position: relative;
+        width: 100%;
+        padding: 20px;
+        display: grid;
+
+        grid-gap: 20px;
+        min-height: 200px;
+    }
+
+    .graphBox .box {
+        position: relative;
+        background: #fff;
+        padding: 20px;
+        width: 100%;
+        box-shadow: 0 7px 25px rgba(0, 0, 0, 0.08);
+        border-radius: 20px;
+    }
+
+    .BoxTable {
+        position: relative;
+        width: 100%;
+        padding: 20px;
+        display: grid;
+        grid-template-columns: 2fr 2fr;
+        grid-gap: 30px;
+        min-height: 200px;
+    }
+
+    .BoxTable .boxt {
+        position: relative;
+        background: #fff;
+        padding: 20px;
+        width: 100%;
+        box-shadow: 0 7px 25px rgba(0, 0, 0, 0.08);
+        border-radius: 20px;
+    }
+
+    @media(max-width: 991px) {
+        .graphBox {
+            grid-template-columns: 1fr;
+            height: auto;
+        }
+    }
 </style>
 
 <body>
@@ -114,21 +160,15 @@ $result_l = mysqli_query($con, $query);
     include "nav_user.php";
     require_once "../app/views/session_status.php";
     ?>
-    <!-- <button onclick="topFunction()" id="myBtn" title="Go to top" style="opacity: 0.5;background-color: #ff5722;width: 50px; height: 50px;"><i class="fas fa-chevron-circle-up"></i></button> -->
-    <!--    background-color: #827A7A; -->
+
     <div>
         <div id="grad" style=" width: auto; height: auto;margin: 15px;border-radius: 7px;padding: 30px; box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 10px;">
 
-            <p style="float: right;">
-                <?php echo $_SESSION['fname_login'] . " " . $_SESSION['lname_login'] ?>
-            </p>
+
             <h2 style="color: #fff;font-family: SUT_Bold; text-shadow:2px 3px 10px #000; ">
-                <i class="fa fa-caret-right" style="font-size:48px  "></i>ยืมครุภัณฑ์
+                <i class="fa fa-caret-right" style="font-size:48px  "></i> &nbsp;&nbsp;&nbsp;ยืมครุภัณฑ์
             </h2>
             <br><br>
-            <!--   1300px -->
-            <!--  <div style="max-width: auto; margin: 15px auto 15px auto;background-color: #b3abab; border-radius: 7px;padding: 30px; box-shadow: rgba(0, 0, 0, 0.35) 0px 0px 10px;"> -->
-            <!--   <h3 style="color: #fff;font-family: SUT_Bold;"><i class="far fa-edit"></i>ทำรายการ</h3> -->
             <form action="../app/models/add_borrow.php" method="post">
                 <? require "../app/controller/scaner.php" ?>
                 <!-- <center><a href="#"> Scan QR Code</a></center> -->
@@ -140,18 +180,44 @@ $result_l = mysqli_query($con, $query);
                 ?>
                 <br>
                 <br>
-
+                <div class="graphBox">
+                    <div class="box" style=" box-shadow: rgba(0, 0.35, 0, 0.35) 0px 0px 15px  ;">
+                        <div class="table-container">
+                            <table id="datatable" class="table" style="text-align: center;">
+                                <thead style="color:white; background-color:#E6581D; ">
+                                    <th>
+                                        <center>ชื่อรายการ</center>
+                                    </th>
+                                    <th>
+                                        <center>ยี่ห้อ/รุ่น</center>
+                                    </th>
+                                    <th>
+                                        <center></center>
+                                    </th>
+                                </thead>
+                                <tbody id="data">
+                                    <p>
+                                        <td colspan="10" class="text-center">แสกน QR Code ยืมครุภัณฑ์</td>
+                                    </p>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
                 <center>
                     <input class="w3-input" type="text" required placeholder="code" id="data4" name="data4" style="max-width: 500px;visibility: hidden; ">
-                    <!-- <h2 id="data4" name="data4">
-                        </h2> -->
-                    <!-- <h2 id="result" ></h2> -->
-                    <!-- <br><br> -->
-                    <!--  //<h2 id="data4" name="data4"></h2>
-                        //<h2 id="result" ></h2>
-                       // <br><br> -->
+
+                    <div class="col-sm-12 col-md-6 mb-3 mb-lg-0 col-lg-4">
+                        <div class="section_grid_bor">
+                            <div class="section_grid_item">
+                                <div style="font-size: 25px;">วันที่คืน*</div>
+                                <input class="w3-input w3-animate-input" style="max-width: 500px; box-shadow: rgba(0.35, 0, 0, 0.35) 0px 5px 10px;" type="date" id="date" name="date" required>
+                            </div>
+                        </div>
+                    </div><br>
                     <div class="col-sm-12 col-md-6 mb-3 mb-lg-0 col-lg-4">
                         <input class="w3-input" type="text" required placeholder="ระบุงานที่จะนำไปใช้ทำกิจกรรม" id="activity" name="activity" style="max-width: 500px; box-shadow: rgba(0.35, 0, 0, 0.35) 0px 5px 10px;">
+
                     </div>
 
                     <div class="col-sm-12 col-md-6 mb-3 mb-lg-0 col-lg-4">
@@ -159,76 +225,15 @@ $result_l = mysqli_query($con, $query);
 
                             <div class="section_grid_bor">
                                 <div class="section_grid_item">
-                                    <!--        //<h5 style="padding-left: 0;">ระบุสถานที่<h5> -->
+
                                     <br>
                                     <input class="w3-input w3-animate-input demo" id="location" name="location" type="text" required placeholder="ระบุสถานที่" style="max-width: 500px; box-shadow: rgba(0.35, 0, 0, 0.35) 0px 5px 10px; ">
-                                </div>
-                            </div>
-                        </div>
-                        <!--    // <p id="demo">CODE</p> -->
-                        <br>
-                        <div class="col-sm-12 col-md-6 mb-3 mb-lg-0 col-lg-4">
-                            <div class="section_grid_bor">
-                                <div class="section_grid_item">
-                                    <div style="font-size: 25px;">วันที่คืน*</div>
-                                    <input class="w3-input w3-animate-input" style="max-width: 500px; box-shadow: rgba(0.35, 0, 0, 0.35) 0px 5px 10px;" type="date" id="date" name="date" required>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </center>
                 <br><br>
-
-                <!--  <div style="max-width: auto;  margin: 15px auto 15px auto;background-color: rgba(255, 255, 255, 0.4);border-radius: 7px;padding: 30px; box-shadow: rgba(0, 0, 0, 0.35) 0px 0px 10px;"> -->
-
-
-                <!--  </div> -->
-                <div style="max-width: 1600px;margin-left: auto;">
-                    <!-- <h2 style="padding-left: 200px;">รายละเอียดการยืม</h2> -->
-                    <table id="datatable" class="table" style="max-width: 1200px;margin: auto; padding: 16px;background-color: white;border-radius: 7px;text-align: center; box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 10px;">
-                        <thead class="table-dark">
-                            <th>
-                                <center>id </center>
-                            </th>
-                            <th>
-                                <center>updateTime</center>
-                            </th>
-                            <th>
-                                <center>itemCode</center>
-                            </th>
-                            <th>
-                                <center>detail</center>
-                            </th>
-                            <th>
-                                <center>checkInDate</center>
-                            </th>
-                            <th>
-                                <center>brand</center>
-                            </th>
-                            <th>
-                                <center>serialNumber</center>
-                            </th>
-                            <th>
-                                <center>price</center>
-                            </th>
-                            <th>
-                                <center>refDoc</center>
-                            </th>
-                            <th>
-                                <center>room</center>
-                            </th>
-                            <th>
-                                <center></center>
-                            </th>
-                        </thead>
-                        <tbody id="data">
-                            <p>
-                                <td colspan="10" class="text-center">แสกน QR Code ยืมครุภัณฑ์</td>
-                            </p>
-                        </tbody>
-                    </table>
-                </div>
-
                 <script>
                     function delAll_test() {
                         // document.getElementById("data4").innerHTML = code234
@@ -244,14 +249,12 @@ $result_l = mysqli_query($con, $query);
 
                 </table>
 
-            </form>
-            <!-- </div> -->
-            <br>
 
+            </form>
+            <br>
         </div>
     </div>
     </form>
-    <!-- </div> -->
     <br>
     </div>
     </div>
