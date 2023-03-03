f1a2e242f0354a32d62c67e38385696d52dfcdbf
phumcherm <kittiphum1112@gmail.com>
2023-03-03 14:55:54 +0700

    Update test.php

--- public/test.php
+++ public/test.php
@@ -5,42 +5,77 @@
     <title>Shopping Cart</title>
     <style>
         table {
+            width: 100%;
             border-collapse: collapse;
-            margin-bottom: 20px;
         }
 
-        table th,
-        table td {
-            border: 1px solid black;
-            padding: 5px;
+        th,
+        td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
         }
 
-        button {
-            margin-right: 10px;
+        @media (max-width: 768px) {
+
+            /* Change table to card at max-width 768px */
+            table {
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: center;
+            }
+
+            tr {
+                display: flex;
+                flex-direction: column;
+                margin: 16px;
+                box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
+                border-radius: 4px;
+                background-color: #fff;
+            }
+
+            th,
+            td {
+                padding: 16px;
+                border: none;
+            }
+
+            th:first-child,
+            td:first-child {
+                border-top-left-radius: 4px;
+                border-top-right-radius: 4px;
+            }
+
+            th:last-child,
+            td:last-child {
+                border-bottom-left-radius: 4px;
+                border-bottom-right-radius: 4px;
+            }
         }
     </style>
 </head>
 
 <body>
-    <button onclick="fetchDataAndPopulateTable()">Fetch Data</button>
-
-    <table id="my-table">
-        <thead>
-            <tr>
-                <th>Column 1</th>
-                <th>Column 2</th>
-                <th>Column 3</th>
-            </tr>
-        </thead>
-        <tbody>
-            <!-- Data rows will be added here dynamically -->
-        </tbody>
+    <table>
+        <tr>
+            <th>Header 1</th>
+            <th>Header 2</th>
+            <th>Header 3</th>
+        </tr>
+        <tr>
+            <td>Row 1, Column 1</td>
+            <td>Row 1, Column 2</td>
+            <td>Row 1, Column 3</td>
+        </tr>
+        <tr>
+            <td>Row 2, Column 1</td>
+            <td>Row 2, Column 2</td>
+            <td>Row 2, Column 3</td>
+        </tr>
     </table>
-    <!-- <div>
-        <h3>Shopping Cart:</h3>
-        <div id="shoppingCart"></div>
-        <p>Total: <span id="totalPrice">0</span></p>
-    </div> -->
+
+
+
     <script>
         // const shoppingCart = {};
 
@@ -91,35 +126,35 @@
         // }
 
 
-        function fetchDataAndPopulateTable() {
-            // Make a request to fetch data from the server
-            fetch('/path/to/data')
-                .then(response => response.json()) // parse the response as JSON
-                .then(data => {
-                    // Get a reference to the HTML table element
-                    const table = document.querySelector('#my-table');
+        // function fetchDataAndPopulateTable() {
+        //     // Make a request to fetch data from the server
+        //     fetch('/path/to/data')
+        //         .then(response => response.json()) // parse the response as JSON
+        //         .then(data => {
+        //             // Get a reference to the HTML table element
+        //             const table = document.querySelector('#my-table');
 
-                    // Create a table row for each data item
-                    const rows = data.map(item => {
-                        // Create a table cell for each property of the data item
-                        const cells = Object.keys(item).map(key => {
-                            const cell = document.createElement('td');
-                            cell.textContent = item[key];
-                            return cell;
-                        });
+        //             // Create a table row for each data item
+        //             const rows = data.map(item => {
+        //                 // Create a table cell for each property of the data item
+        //                 const cells = Object.keys(item).map(key => {
+        //                     const cell = document.createElement('td');
+        //                     cell.textContent = item[key];
+        //                     return cell;
+        //                 });
 
-                        // Create a table row and add the cells to it
-                        const row = document.createElement('tr');
-                        cells.forEach(cell => row.appendChild(cell));
-                        return row;
-                    });
+        //                 // Create a table row and add the cells to it
+        //                 const row = document.createElement('tr');
+        //                 cells.forEach(cell => row.appendChild(cell));
+        //                 return row;
+        //             });
 
-                    // Add the rows to the table
-                    table.innerHTML = '';
-                    rows.forEach(row => table.appendChild(row));
-                })
-                .catch(error => console.error(error));
-        }
+        //             // Add the rows to the table
+        //             table.innerHTML = '';
+        //             rows.forEach(row => table.appendChild(row));
+        //         })
+        //         .catch(error => console.error(error));
+        // }
     </script>
 </body>
 
